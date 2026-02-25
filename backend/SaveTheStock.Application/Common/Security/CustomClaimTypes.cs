a7a8860270ac34522db59b4743dd50075ea9a257
CamFav <camillefvrl@gmail.com>
2026-02-25 11:13:32 +0100

    feat: add CustomClaimTypes constants for JWT claims

---
+++ backend/SaveTheStock.Application/Common/Security/CustomClaimTypes.cs
@@ -0,0 +1,7 @@
+namespace SaveTheStock.Application.Common.Security;
+
+public static class CustomClaimTypes
+{
+    public const string AccountId = "account_id";
+    public const string CompanyId = "company_id";
+}
